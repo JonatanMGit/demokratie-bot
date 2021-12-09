512628e27efbc6d2eec97fda2544af721bc06523
Jonatan_M <60609358+JonatanMGit@users.noreply.github.com>
2021-12-09 22:23:05 +0100

    Create vote Command

---
+++ src/commands/vote.ts
@@ -0,0 +1,9 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('vote').setDescription('Create a vote'),
+	async execute(interaction) {
+		return interaction.reply('This Command hasn\'t been implemented yet!');
+	},
+	name: 'vote',
+};
